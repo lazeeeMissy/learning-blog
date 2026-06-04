6b755abb54dd53b504e5b5c094d94cbe76ab833b
lazeeeMissy <missylazy1121@gmail.com>
2026-06-04 10:16:59 +1000

    remove all

--- lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(value: string) {
-  return new Intl.DateTimeFormat("en", {
-    year: "numeric",
-    month: "short",
-    day: "numeric"
-  }).format(new Date(value));
-}
